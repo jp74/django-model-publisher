30228542b38a2d29e60074d1eddda73e49d96ef9
Mishbah <mishbah@jp74.com>
2014-06-05 13:13:15 +0100

    Typo allow_tags = True

--- publisher/admin.py
+++ publisher/admin.py
@@ -121,7 +121,7 @@ class PublisherAdmin(ModelAdmin):
         })
         return t.render(c)
     publisher_status.short_description = 'Last Changes'
-    publisher_status.allow_tag = True
+    publisher_status.allow_tags = True
 
     def publisher_publish(self, obj):
 
@@ -140,7 +140,7 @@ class PublisherAdmin(ModelAdmin):
         })
         return t.render(c)
     publisher_publish.short_description = 'Published'
-    publisher_publish.allow_tag = True
+    publisher_publish.allow_tags = True
 
     def queryset(self, request):
         self.request = request
