cba266702258f0a8c67efd3118667d8a0005692f
Ashley Wilson <ash@jp74.com>
2014-07-08 09:56:12 +0100

    Swapped from data attributes to standard attr call to fix undefined bug

--- publisher/static/publisher/publisher.js
+++ publisher/static/publisher/publisher.js
@@ -1,16 +1,15 @@
 var $ = django.jQuery;
 $(function() {
-    $('input:checkbox.publish-checkbox').change(function() {
-        if ($(this).is(':checked')) {
-            var url = $(this).data('publish');
-        }
-        else {
-            var url = $(this).data('unpublish');
-        }
-        $.get(url, function(data) {
-			if (data.success) {
-				$('.published-icon').find('img').toggle();
-			}
-		});
-    })
-})
+  $('input:checkbox.publish-checkbox').change(function() {
+    if ($(this).is(':checked')) {
+      var url = $(this).attr('data-publish');
+    } else {
+      var url = $(this).attr('data-unpublish');
+    }
+    $.get(url, function(data) {
+      if (data.success) {
+        $('.published-icon').find('img').toggle();
+      }
+    });
+  });
+});
