f3345ffad6d73999d0f2cffec21a48f5b6b1d68e
Fabien Wald <fabien@jp74.com>
2014-07-10 15:29:49 +0100

    Small code simplification.

--- publisher/middleware.py
+++ publisher/middleware.py
@@ -7,7 +7,7 @@ class PublisherMiddleware(object):
     @staticmethod
     def is_draft(request):
         authenticated = request.user.is_authenticated() and request.user.is_staff
-        is_draft = True if request.GET and 'edit' in request.GET and authenticated else False
+        is_draft = 'edit' in request.GET and authenticated
         return is_draft
 
     def process_request(self, request):
