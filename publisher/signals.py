c34b6f2d49b77857318b0f490f233bcd0383b8be
Fabien Wald <fabien@jp74.com>
2015-08-12 15:43:33 +0100

    Typo.

--- publisher/signals.py
+++ publisher/signals.py
@@ -18,7 +18,7 @@ def publisher_pre_delete(sender, **kwargs):
 publisher_pre_publish = Signal(providing_args=['instance'])
 
 
-# Sent when a model is being published, before the draft is saved (the draft is sent)
+# Sent when a model is being published, before the draft is saved (the draft is sent).
 publisher_publish_pre_save_draft = Signal(providing_args=['instance'])
 
 
@@ -26,9 +26,9 @@ publisher_publish_pre_save_draft = Signal(providing_args=['instance'])
 publisher_post_publish = Signal(providing_args=['instance'])
 
 
-# Sent when a model about to be unpublished (the draft is sent)
+# Sent when a model is about to be unpublished (the draft is sent).
 publisher_pre_unpublish = Signal(providing_args=['instance'])
 
 
-# Sent when a model is unpublished (the draft is sent)
+# Sent when a model is unpublished (the draft is sent).
 publisher_post_unpublish = Signal(providing_args=['instance'])
