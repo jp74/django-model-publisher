beb4481ac35dc58626bd32fed10870d6b69b310c
JensDiemer <git@jensdiemer.de>
2017-09-04 11:15:30 +0200

    Use .visible() in views

--- publisher/views.py
+++ publisher/views.py
@@ -1,8 +1,6 @@
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
-from .middleware import get_draft_status
-
 
 class PublisherViewMixin(object):
 
@@ -10,7 +8,7 @@ class PublisherViewMixin(object):
         abstract = True
 
     def get_queryset(self):
-        return self.model.objects.filter(publisher_is_draft=get_draft_status()).all()
+        return self.model.objects.visible()
 
 
 class PublisherDetailView(PublisherViewMixin, DetailView):
