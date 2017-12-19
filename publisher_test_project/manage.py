ab3b2e9a681740159897ef8592c4f891df791119
JensDiemer <git@jensdiemer.de>
2017-12-19 10:45:46 +0100

    Raise error if virtualenv not activated

--- publisher_test_project/manage.py
+++ publisher_test_project/manage.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import os
 import sys
 
+print("sys.real_prefix:", getattr(sys, "real_prefix", "-"))
+print("sys.prefix:", sys.prefix)
+
 if __name__ == "__main__":
+    if "VIRTUAL_ENV" not in os.environ:
+        raise RuntimeError(" *** ERROR: Virtual env not activated! *** ")
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
