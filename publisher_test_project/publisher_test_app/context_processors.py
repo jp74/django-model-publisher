b85f788ef743939243eaa307634e921672d3cdc7
JensDiemer <git@jensdiemer.de>
2017-12-28 12:23:14 +0100

    add django version to page

--- publisher_test_project/publisher_test_app/context_processors.py
+++ publisher_test_project/publisher_test_app/context_processors.py
@@ -1,8 +1,10 @@
 
+import django
 
 def debug_info(request):
     user = request.user
     return {
+        "django_version": django.__version__,
         "cms_change_page": user.has_perms("cms.change_page"),
         "cms_publish_page": user.has_perms("cms.publish_page"),
     }
