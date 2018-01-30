73a8abe06887f062fff29d1f963d3752d46084e4
JensDiemer <git@jensdiemer.de>
2018-01-30 07:54:22 +0100

    List usernames

--- publisher_test_project/publisher_test_app/context_processors.py
+++ publisher_test_project/publisher_test_app/context_processors.py
@@ -1,10 +1,16 @@
 
 import django
+from django.contrib.auth import get_user_model
+
 
 def debug_info(request):
+    User = get_user_model()
+    usernames = User.objects.filter(is_staff=True, is_active=True).values_list("username", flat=True)
+
     user = request.user
     return {
         "django_version": django.__version__,
         "cms_change_page": user.has_perms("cms.change_page"),
         "cms_publish_page": user.has_perms("cms.publish_page"),
+        "usernames": usernames,
     }
